24bb02c94a11472b91d68413bf857531e732e1fd
BuildTools <unconfigured@null.spigotmc.org>
2023-02-08 21:13:42 +0700

    Auto-generated commit [ci skip]

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+function preLoad() {
+    /*const loader = document.querySelector(".preload");
+    if (loader === null) {
+        throw new Error();
+    }
+    window.addEventListener('load', function() {
+        loader.style.display = "none";
+    });*/
+    /*window.onload = function() {
+        document.querySelector(".preload").style.display = "none";
+    }*/
+    alert('Hello');
+}
---
+++ scripts.341c72feb497de4b.js
@@ -0,0 +1,1 @@
+function preLoad(){alert("Hello")}
